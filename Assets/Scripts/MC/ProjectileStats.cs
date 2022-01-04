d07a82900a568761ef1414a07aca422ce5041850
Rohaid <rohaidb1999@gmail.com>
2022-01-03 23:31:29 -0500

    Projectile Functionality

---
+++ Assets/Scripts/MC/ProjectileStats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ProjectileStats", menuName = "MC/ProjectileStats")]
+public class ProjectileStats : ScriptableObject
+{
+    public string projectileName;
+    public int damageAmount;
+    public float attackCoolDown;
+     public float speed;
+     public float distance;
+     public float lifeTime;
+     public AudioClip launch;
+}
