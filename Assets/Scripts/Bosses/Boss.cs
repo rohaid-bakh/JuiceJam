a5b17f50e4966605fdcb05a2e612d3e2cbf5f009
Rohaid <rohaidb1999@gmail.com>
2022-01-04 23:18:34 -0500

    Added Hit Flash
    - To Boss Hit
    - To Player Hit

--- Assets/Scripts/Bosses/Boss.cs
+++ Assets/Scripts/Bosses/Boss.cs
@@ -6,15 +6,21 @@ public class Boss : MonoBehaviour
 {
     [SerializeField] private BossStats _stats;
     [SerializeField] private float CurrHealth;
-
+    [SerializeField] private Material hitFlash;
+    private SpriteRenderer renderer;
+    private Material defaultMaterial;
+    [SerializeField] private float flashDuration;
     void Awake()
     {
         CurrHealth = _stats.healthAmount;
+        renderer = GetComponent<SpriteRenderer>();
+        defaultMaterial = renderer.material;
     }
 
     public void takeDamage(float damage)
     {
         CurrHealth -= damage;
+        StartCoroutine(HitFlash());
         if (CurrHealth <= 0)
         {
            DestroySelf();
@@ -23,5 +29,12 @@ public class Boss : MonoBehaviour
 
     private void DestroySelf(){
         Destroy(gameObject);
+    }
+
+    private IEnumerator HitFlash(){
+        renderer.material = hitFlash;
+        yield return new WaitForSeconds(flashDuration);
+        renderer.material = defaultMaterial;
+
     }
 }
