8c54d8b0fdce5a38ead9e8f833ab5ddc5f7b5195
Loffy <edwina.doe123@gmail.com>
2022-01-10 22:13:07 +0100

    Update to last commit.

--- Assets/Scripts/Bosses/Kraken/KrakenController.cs
+++ Assets/Scripts/Bosses/Kraken/KrakenController.cs
@@ -9,43 +9,12 @@ public class KrakenController : MonoBehaviour
     private SpriteRenderer renderer;
     private Material defaultMaterial;
 
-    [SerializeField] private float movementCooldown = 1f;
-    Vector2 position1 = new Vector2(-60, 0);
-    Vector2 position2 = new Vector2(60, 0);
-    [SerializeField] float speed = 3.0f;
-    int direction = 1;
-    bool movement = true;
-
     void Awake()
     {
         CurrHealth = _stats.healthAmount;
         renderer = GetComponent<SpriteRenderer>();
         defaultMaterial = renderer.material;
     }
-
-    void FixedUpdate()
-    {
-        if (movement)
-        {
-            switch (direction)
-            {
-                case -1:
-                    transform.position = Vector2.MoveTowards(transform.position, position1, speed * Time.deltaTime);
-                    movement = false;
-                    direction = 1;
-                    StartCoroutine(MovementCooldown());
-                    break;
-
-                case 1:
-                    transform.position = Vector2.MoveTowards(transform.position, position2, speed * Time.deltaTime);
-                    movement = false;
-                    direction = -1;
-                    StartCoroutine(MovementCooldown());
-                    break;
-            }
-        }
-    }
-
     public void takeDamage(float damage)
     {
         CurrHealth -= damage;
@@ -60,12 +29,6 @@ public class KrakenController : MonoBehaviour
     {
         Instantiate(_stats.BossExplosion, transform.position, transform.rotation);
         Destroy(gameObject);
-    }
-
-    private IEnumerator MovementCooldown()
-    {
-        yield return new WaitForSeconds(movementCooldown);
-        movement = true;
     }
 
         private IEnumerator HitFlash()
